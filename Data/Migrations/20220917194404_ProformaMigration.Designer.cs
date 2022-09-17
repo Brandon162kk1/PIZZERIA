406439fd9c216bb7313495dc38d07ce701c2bc1a
Brandon Rodriguez <brandon162001@gmail.com>
2022-09-17 15:08:24 -0500

    Add cambios

--- Data/Migrations/20220917194404_ProformaMigration.Designer.cs
+++ Data/Migrations/20220917194404_ProformaMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PIZZERIA.Data;
@@ -11,9 +12,10 @@ using PIZZERIA.Data;
 namespace PIZZERIA.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220917194404_ProformaMigration")]
+    partial class ProformaMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
